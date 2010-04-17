25ee671efcf17794dd5a75e711ae66d4242b2d4c
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-04-17 17:28:29 +0100

    Rendering the welcome page using Haml

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 var sys = require('sys'),
-   http = require('http');
+        http = require('http'),
+        Haml = require('../lib/haml'),
+        File = require('fs');
 
 var insight = {};
 
 insight.server = function() {
     var that = {};
 
+    var renderWelcomePageTo = function(response) {
+        File.readFile('../../templates/index.haml', "UTF-8", function (err, haml) {
+            var data = {
+                title: "Insight",
+                contents: "<h1>Welcome to Insight</h1>"
+            };
+            page = Haml.render(haml, {locals: data});
+            response.end(page);
+        });
+    };
+
     var server = http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Welcome to Insight');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        renderWelcomePageTo(res);
     });
 
     that.start = function() {
