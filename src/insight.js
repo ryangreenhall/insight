91d99dab0d0f48331cf57ef160199e19c911f046
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-05-26 14:18:16 +0100

    get now takes a callback that can be used to process the response.

--- src/insight.js
+++ src/insight.js
@@ -1,76 +1,58 @@
 var insight = {};
 
 var sys = require('sys'),
-http = require('http'),
-File = require('fs'),
-Url = require('url'),
-underscore = require("../lib/underscore");
+        http = require('http'),
+        File = require('fs'),
+        Url = require('url'),
+        underscore = require("../lib/underscore");
 
 
 var kiwi = require('kiwi');
 kiwi.require('express');
 
 configure(function() {
-  set('root', __dirname);
+    set('root', __dirname);
 });
 
 insight.resource = function(url) {
     var that = {};
-    that.get = function() {
-        sys.puts("GET: " + url);
+    var parsedUrl = Url.parse(url, false);
+    that.get = function(callback) {
+
+        var client = http.createClient(parsedUrl.port, parsedUrl.hostname);
+        var request = client.request('GET', parsedUrl.pathname, {'host': parsedUrl.hostname});
+        request.addListener('response', function (response) {
+            response.setEncoding('utf8');
+            response.addListener('data', function (chunk) {
+                callback(chunk);
+            });
+        });
+        request.end();
     };
     return that;
 };
 
-get('/', function(){
-  return this.render('index.html.haml', {
-                      layout: false,
-                        locals: {
-                          title: 'Insight',
-                          config: insight.config().load()
-                        }
-                    });
+get('/', function() {
+    return this.render('index.html.haml', {
+        layout: false,
+        locals: {
+            title: 'Insight',
+            config: insight.config().load()
+        }
+    });
 });
 
 get('/status/:environment', function(environment) {
-    // load the configuration for the given environment
+
     var config = insight.config().load();
     var env = config.environments[environment];
 
-    var requestStatus = function(url) {
-        insight.resource(url).get(); 
-
-        var url = Url.parse(url, false);
-      
-        var google = http.createClient(url.port, url.hostname);
-        var request = google.request('GET', url.pathname,
-          {'host': url.hostname});
-
-        var statusAsJson = {};
-        request.addListener('response', function (response) {
-          response.setEncoding('utf8');
-          response.addListener('data', function (chunk) {
-            statusAsJson.status = chunk;
-            sys.puts('BODY: ' + chunk);
-            sys.puts('More body' + statusAsJson.status);
-          });
+    var status = [];
+    env.urls.forEach(function(url) {
+        insight.resource(url).get(function(data) {
+            sys.puts("This is the data I have: " + data);
         });
-        request.end();
-        return statusAsJson.status;
-    };
-
-
-    _.each(env.urls, function(url){
-
-        // request the url and parse the JSON.
-        var statusAsJson = requestStatus(url);
-        sys.puts(statusAsJson);
-
     });
-    
-    
-
-    // request the status page for each
 
     // write the contents.
     return "status of " + environment;
