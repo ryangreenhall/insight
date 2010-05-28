ad901587e27535ed055e26cc8feade150c4235f6
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-05-28 10:51:21 +0100

    CSS is now served from the public dir using the static file handler.

--- src/insight.js
+++ src/insight.js
@@ -104,11 +104,6 @@ get('/status/:environment', function(environment) {
     });
 });
 
-get('/*.css', function(file) {
-    var toRead = __dirname + '/' + file + '.css';
-    return File.readFileSync(toRead);
-});
-
 insight.config = function() {
     var that = {};
     var config;
