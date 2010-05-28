39c4298b4bf7a170f4fd1493eb7fbc04c462cea5
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-05-28 10:06:40 +0100

    Detecting when a server is unavailable so that we can provide a warning on the dashboard.

--- src/insight.js
+++ src/insight.js
@@ -26,14 +26,14 @@ sys.inherits(EventBroker, Events.EventEmitter);
 
 var eventBroker = new EventBroker();
 
-eventBroker.addListener("status-retrieval-complete", function(environment, states, request, config){
+eventBroker.addListener("status-retrieval-complete", function(environment, states, request, config, propertyNames) {
     return request.render('dashboard.html.haml', {
         locals: {
             title: environment + ' Dashboard',
             environment: environment,
             states: states,
-            config: config
-
+            config: config,
+            propertyNames: propertyNames
         }
     });
 });
@@ -43,16 +43,18 @@ insight.resource = function(url) {
     var that = {};
     var parsedUrl = Url.parse(url, false);
     that.get = function(callback) {
-
         var client = http.createClient(parsedUrl.port, parsedUrl.hostname);
         var request = client.request('GET', parsedUrl.pathname, {'host': parsedUrl.hostname});
         request.addListener('response', function (response) {
             response.setEncoding('utf8');
             response.addListener('data', function (chunk) {
                 callback(chunk);
-
             });
         });
+        client.addListener('error', function (err) {
+            callback("{\"isUnavailable\":\"true\"}");
+        });
+
         request.end();
     };
     return that;
@@ -75,20 +77,42 @@ get('/status/:environment', function(environment) {
     var states = [];
     var that = this;
     env.urls.forEach(function(url) {
+
+        var propertyNamesFrom = function(healthyServerState) {
+            var propertyNames = [];
+            for (property in healthyServerState) {
+                propertyNames.push(property);
+            }
+            return propertyNames;
+        };
+
         insight.resource(url).get(function(data) {
             var status = JSON.parse(data);
             status.server = url;
             states.push(status);
+
+
+            var healthyServerStates = states.filter(function(status){
+                return !status.isUnavailable;
+            });
+
+            var propertyNames = propertyNamesFrom(healthyServerStates[0]);
+//            propertyNames.forEach(function(propertyName){
+//                sys.puts(propertyName);
+//            });
+
+            sys.puts("Healthy servers: " + healthyServerStates.length);
+
             if (states.length === env.urls.length) {
-                eventBroker.emit("status-retrieval-complete", environment, states, that, config);
+                eventBroker.emit("status-retrieval-complete", environment, states, that, config, propertyNames);
             }
         });
     });
 });
 
-get('/*.css', function(file){
-  var toRead = __dirname + '/' + file + '.css';
-  return File.readFileSync(toRead);
+get('/*.css', function(file) {
+    var toRead = __dirname + '/' + file + '.css';
+    return File.readFileSync(toRead);
 });
 
 insight.config = function() {
--- src/resource.js
+++
@@ -1,7 +0,0 @@
-insight.resource = function(url) {
-    var that = {};
-    that.get = function() {
-        sys.puts("GET: " + url);
-    };
-    return that;
-};
