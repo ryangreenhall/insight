23fcd1553a765af4ed70a05f8b7857446746313b
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-06-01 12:54:12 +0100

    More config parsing specs.

--- spec/config.spec.js
+++ spec/config.spec.js
@@ -5,10 +5,22 @@ sys = require('sys');
 require('config');
 
 describe('config', function(){
-  it('can be loaded from file', function(){
-      var config = insight.config().load();
-      expect(config.environments.Prod.urls).toContain("http://localhost:8080/public/status.json");
-      expect(config.environments.Prod.urls).toContain("http://localhost:8081/public/status.json");
-      expect(config.environments.Prod.urls).toContain("http://localhost:8086/public/status.json");
-  });
+
+    it('can be loaded from file', function(){
+        var config = insight.config().load();
+        expect(config.environments.Prod.urls).toEqual(
+                ['http://localhost:8080/public/status.json',
+                 'http://localhost:8081/public/status.json',
+                 'http://localhost:8086/public/status.json']);
+    });
+
+    it("contains three environments", function(){
+        var config = insight.config().load();
+
+        var envs = [];
+        for (environment in config.environments) {
+            envs.push(environment);
+        }
+        expect(envs).toEqual(["Staging", "Showcase", "Prod"]);
+    });
 });
