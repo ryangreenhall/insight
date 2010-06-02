2894e953670c1ba2424eae629f021611349adb39
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-06-02 12:30:47 +0100

    Introducing page abstraction for the homepage.

--- scenarios/environments_are_listed_on_home_page_scenario.rb
+++ scenarios/environments_are_listed_on_home_page_scenario.rb
@@ -5,21 +5,30 @@ require "spec"
 describe "HomePage" do
 
     it "should list the environments" do
-        browser = Celerity::Browser.new
-        browser.goto("http://localhost:3000")
-
-        links = browser.links
-
-        environments = []
-        links.each do |link|
-           environments << link.text
-        end
-
-        environments.should eql ["Staging", "Showcase", "Prod"]
+        homepage = HomePage.new
+        homepage.load
+        homepage.get_environments.should eql ["Staging", "Showcase", "Prod"]
     end
 
+    class HomePage 
+
+        def initialize
+            @browser = Celerity::Browser.new
+        end
+
+        def load
+            @browser.goto("http://localhost:3000")
+        end
+
+        def get_environments
+            links = @browser.links
+
+            environments = []
+            links.each do |link|
+               environments << link.text
+            end
+            return environments
+        end
+    end
 end
-
-
-
 
