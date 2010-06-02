916cb405cab1d19f8bb1f5d502db37c7e3ec6c8f
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-06-02 16:12:16 +0100

    More scenarios ensuring the correct heading is displayed on the various dashboards.

--- scenarios/dashboard_scenario.rb
+++ scenarios/dashboard_scenario.rb
@@ -13,6 +13,18 @@ describe "Dashboard" do
         dashboard.heading.should eql "Prod"
     end
 
+    it "Showcase dashboard has Showcase as the heading" do
+        home = navigate_to_home
+        dashboard = home.navigate_to_dashboard "Showcase"
+        dashboard.heading.should eql "Showcase"
+    end
+
+    it "Staging dashboard has Staging as the heading" do
+        home = navigate_to_home
+        dashboard = home.navigate_to_dashboard "Staging"
+        dashboard.heading.should eql "Staging"
+    end
+
     it "should provide a warning when a server is dead" do
         home = navigate_to_home
         dashboard = home.navigate_to_dashboard "Prod"
