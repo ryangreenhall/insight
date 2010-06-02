57baca476a3ea0e9fb5e133be8107017019cd078
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-06-02 16:05:34 +0100

    Slight rewording of spec.

--- scenarios/dashboard_scenario.rb
+++ scenarios/dashboard_scenario.rb
@@ -13,10 +13,9 @@ describe "Dashboard" do
         dashboard.heading.should eql "Prod"
     end
 
-    it "should provide a warning indicating that a server is dead" do
+    it "should provide a warning when a server is dead" do
         home = navigate_to_home
         dashboard = home.navigate_to_dashboard "Prod"
-        
         dashboard.server_is_dead?.should eql true
     end
 
