2f221288b2d3f19c717d5106431882e3a89aa549
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-06-09 11:49:22 +0100

    Added a simple refresh mechanism using JavaScript

---
+++ src/public/js/refresh.js
@@ -0,0 +1,9 @@
+function refresh() {
+    location.reload(true);
+};
+
+var refreshEvery60Seconds = function() {
+    var sixtySecondsAsMillis = 60*1000;
+    setInterval("refresh()", sixtySecondsAsMillis);
+};
+
