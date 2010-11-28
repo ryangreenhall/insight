72d9ec4d4e83cd4e41006ff68460f6840e5bc766
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-11-28 16:23:38 +0000

    Starting from scratch for v2.

--- src/public/js/refresh.js
+++
@@ -1,9 +0,0 @@
-function refresh() {
-    location.reload(true);
-};
-
-var refreshEvery60Seconds = function() {
-    var sixtySecondsAsMillis = 60*1000;
-    setInterval("refresh()", sixtySecondsAsMillis);
-};
-
