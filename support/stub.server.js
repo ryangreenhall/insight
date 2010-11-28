42b428e8c603e340a362624063d92b2757bcfad9
Ryan Greenhall <ryan.greenhall@gmail.com>
2010-11-28 19:39:29 +0000

    Added stub server to generate json.

---
+++ support/stub.server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var fs = require('fs')
+
+
+var app = function(port) {
+  var that = {};
+  
+  var app = express.createServer();
+
+  app.configure(function(){
+    app.use(express.staticProvider(__dirname + '/public'));
+  });
+  
+  that.start = function() {
+    app.listen(port)
+  }
+  
+  return that;
+}
+
+
+var ports = [8080, 8081, 8082];
+ports.forEach(function(port) {
+  app(port).start();
+})
