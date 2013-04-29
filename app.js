553011a8d40e151d433a7824a8abd48ef7398cd3
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-04-29 11:48:45 +0100

    We can now specify the port and config file using command line args

--- app.js
+++ app.js
@@ -4,59 +4,69 @@ var express = require('express'),
     events = require('events'),
     sys = require('sys');
     statusAggregator = require('./lib/app.status.aggregator');
+    
+DEFAULT_CONFIG_PATH = "config/config.json";
 
-var app = module.exports = express();
-
-var environments = config.load().environments;
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-function EventBroker() {
-  events.EventEmitter.call(this);
-}
-
-sys.inherits(EventBroker, events.EventEmitter);
-var eventBroker = new EventBroker();
-
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Insight',
-    environments: environments
-  });
-});
-
-app.get('/status/:environment', function(req, res) {
+exports.start = function(port, configPath) {
+  var thePort = port || 3000;
   
-  eventBroker.addListener("status-retrieval-complete", function(environment, states, request, config, propertyNames) {
-    res.render('dashboard', {
-      title: 'Dashboard',
-      environments: environments,
-      environment: environment,
-      config: config,
-      states: states,
-      propertyNames: propertyNames
+  console.log("Insight running on port %d", thePort);
+  
+  var app = express();
+
+  var environments = config.load().environments;
+
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
+
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler()); 
+  });
+
+  function EventBroker() {
+    events.EventEmitter.call(this);
+  }
+
+  sys.inherits(EventBroker, events.EventEmitter);
+  var eventBroker = new EventBroker();
+
+
+  app.get('/', function(req, res){
+    res.render('index', {
+      title: 'Insight',
+      environments: environments
     });
   });
-  
-  statusAggregator.appStatusAggregator(config.load(), eventBroker, this).aggregate(req.params.environment);  
-});
 
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Insight running on port %d", port);
+  app.get('/status/:environment', function(req, res) {
+
+    eventBroker.addListener("status-retrieval-complete", function(environment, states, request, config, propertyNames) {
+      res.render('dashboard', {
+        title: 'Dashboard',
+        environments: environments,
+        environment: environment,
+        config: config,
+        states: states,
+        propertyNames: propertyNames
+      });
+    });
+
+    statusAggregator.appStatusAggregator(config.load(), eventBroker, this).aggregate(req.params.environment);  
+  });
+  
+  app.listen(thePort);
+};
+
+
+
+
